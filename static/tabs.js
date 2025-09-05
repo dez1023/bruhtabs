08e6847bbff36f78c8c7b59a5e2fc67add0051d3
dez1023 <123430228+dez1023@users.noreply.github.com>
2025-09-04 18:27:04 -0700

    Create tabs.js

---
+++ static/tabs.js
@@ -0,0 +1,61 @@
+let currentTab = null;
+const tabs = [];
+
+function openTab(tab) {
+  if (currentTab == tab) {return};
+  if (currentTab) {currentTab.classList.remove("tabOpen");}
+  displayPage(tab.dataset.url);
+  setTimeout(function() {
+      tab.classList.add("tabOpen");
+      currentTab = tab;
+  }, 50)
+}
+
+function updateTab(tab, url) {
+  tab.dataset.url = url;
+  
+  if (pages[url] && pages[url].title) {
+    tab.querySelector("a").textContent = pages[url].title;
+  }
+  
+  if (currentTab == tab) {displayPage(url);}
+}
+
+function newTab(url) {
+  const tab = document.createElement("div");
+  const tabTitle = document.createElement("a");
+  const closeBtn = document.createElement("button");
+  tab.classList.add("tab");
+  closeBtn.textContent = "x";
+  tabBar.appendChild(tab);
+  tab.appendChild(tabTitle);
+  tab.appendChild(closeBtn);
+  updateTab(tab, url ?? "bruh-tabs://")
+  tabs.push(tab);
+  
+  const clickListener = tab.addEventListener("click", function() {
+    event.stopPropagation();
+    openTab(tab);
+  });
+  
+  const closeListener = closeBtn.addEventListener("click", function() {
+    event.stopPropagation();
+    tab.removeEventListener("click", clickListener);
+    closeBtn.removeEventListener("click", closeListener);
+    const index = tabs.indexOf(tab);
+    tabs.splice(index, 1)
+    
+    if (currentTab == tab) {
+      const nextTab = tabs[Math.max(Math.min(index-1, tabs.length), 0)];
+      if (nextTab) {
+        openTab(nextTab);
+      }else{
+        newTab();
+      }
+    }
+    
+    tab.remove();
+  });
+  
+  openTab(tab);
+}
