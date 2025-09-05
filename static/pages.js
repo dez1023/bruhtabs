8d00727f2169bfa47a96ccef4d2b3df93ef740ab
dez1023 <123430228+dez1023@users.noreply.github.com>
2025-09-04 18:27:40 -0700

    Create pages.js

---
+++ static/pages.js
@@ -0,0 +1,37 @@
+let currentPage = null;
+
+const pages = {
+  "bruh-tabs://": {
+    "page": document.getElementById("bruh-tabs_home"),
+    "title": "New Bruhtab",
+  },
+  "bruh-tabs://settings": {
+    "page": document.getElementById("bruh-tabs_settings"),
+    "title": "Bruhtabs - Settings",
+  },
+};
+
+function createPage(url) {
+  const pageElement = document.createElement("div");
+  const link = document.createElement("a");
+  pageDisplay.appendChild(pageElement);
+  pageElement.appendChild(link);
+  link.href = url;
+  link.textContent = "Go to "+url;
+  
+  pages[url] = {
+    "page": pageElement,
+    "title": "New Page",
+  }
+}
+
+function displayPage(url) {
+  const pageData = pages[url];
+  if (!pageData) {return};
+  const page = pageData.page;
+  if (currentPage == page) {return};
+  if (currentPage) {currentPage.style.display = "none";}
+  page.style.display = "block";
+  currentPage = page;
+  addressBar.value = url;
+}
